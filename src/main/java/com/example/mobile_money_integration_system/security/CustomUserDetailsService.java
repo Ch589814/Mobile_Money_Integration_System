e36c8594eaad3f90dcccbf54c5482abe4cf3a4a9
Ch589814 <niyomufashadelphine1@gmail.com>
2026-03-28 20:01:20 +0200

    add CustomerUserDetailsService.java

---
+++ src/main/java/com/example/mobile_money_integration_system/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.mobile_money_integration_system.security;
+import com.example.mobile_money_integration_system.entity.User;
+import com.example.mobile_money_integration_system.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                user.getRoles().stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toSet())
+        );
+    }
+}
